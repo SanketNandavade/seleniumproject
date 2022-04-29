5a79b51dff8d1b7911e553698b4f252477675c67
Sanket <Sanket@Sanket>
2022-04-29 20:44:27 +0530

    2nd upload

---
+++ src/main/java/pom/loginpage.java
@@ -0,0 +1,15 @@
+package pom;
+
+public class loginpage {
+	
+	public void m1() 
+	{
+		System.out.println("running method 1");
+	}
+	
+	public void m2() 
+	{
+	 	System.out.println("running method 2");
+	}
+
+}
